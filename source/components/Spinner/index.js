6d8b3d89cb5dc1a593fcbf7f45db64cf81adc170
Iryna Komarenets <iryna.komarenets@gmail.com>
2019-03-04 22:51:17 +0200

    Validate prop types in Spinner component

--- source/components/Spinner/index.js
+++ source/components/Spinner/index.js
@@ -1,13 +1,15 @@
 // Core
 import React, { Component } from 'react';
-// import { createPortal } from 'react-dom';
+import { bool } from 'prop-types';
 
 // Instruments
 import Styles from './styles.m.css';
 
-// const portal = document.getElementById('spinner');
-
 export default class Spinner extends Component {
+    static propTypes = {
+        isSpinning: bool.isRequired,
+    }
+
     render() {
         const { isSpinning } = this.props;
 
